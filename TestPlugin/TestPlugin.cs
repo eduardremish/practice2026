5e98ee318ff7ed58fec28a2b6689d9e48ce7716b
eduardremish <eduardremish@gmail.com>
2026-07-10 22:46:42 +0300

    Add task10

---
+++ TestPlugin/TestPlugin.cs
@@ -0,0 +1,14 @@
+﻿using CommandLib;
+using System;
+
+namespace TestPlugin
+{
+    [PluginLoad("")]
+    public class AnalyticsCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Тестовый плагин.");
+        }
+    }
+}
---
+++ task10tests/PluginApp.Tests.cs
@@ -0,0 +1,67 @@
+﻿using CommandLib;
+using PluginApp;
+using System.Reflection;
+using Xunit;
+
+namespace PluginApp.Tests
+{
+    public class PluginLoaderTests
+    {
+        [Fact]
+        public void LoadPlugins_ShouldLoadAllPluginsFromDirectory()
+        {
+
+            var loader = new PluginLoader();
+
+            string pluginsDir = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "..", "..", "..", "..",
+                "PluginApp", "bin", "Debug", "net10.0", "plugins"
+            );
+
+
+            if (!Directory.Exists(pluginsDir))
+            {
+                Directory.CreateDirectory(pluginsDir);
+
+
+                string sourceDll = Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    "..", "..", "..", "..",
+                    "TestPlugin", "bin", "Debug", "net10.0", "TestPlugin.dll"
+                );
+
+                if (File.Exists(sourceDll))
+                {
+                    File.Copy(sourceDll, Path.Combine(pluginsDir, "TestPlugin.dll"), true);
+                }
+            }
+
+
+            var plugins = loader.LoadPlugins(pluginsDir);
+
+
+            Assert.NotEmpty(plugins);
+        }
+
+        [Fact]
+        public void LoadPlugins_ShouldReturnEmpty_WhenNoDlls()
+        {
+
+            var loader = new PluginLoader();
+            string emptyDir = Path.Combine(Path.GetTempPath(), "empty_plugins_" + Guid.NewGuid().ToString());
+            Directory.CreateDirectory(emptyDir);
+
+
+            var plugins = loader.LoadPlugins(emptyDir);
+
+
+            Assert.Empty(plugins);
+
+
+            Directory.Delete(emptyDir, true);
+        }
+
+    }
+}
+
