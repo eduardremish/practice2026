f7edb0417edcd79ab975966860cc0b16b4cb697e
eduardremish <eduardremish@gmail.com>
2026-07-14 10:38:43 +0300

    Task14

---
+++ task14tests/IntegralTests.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+using Xunit;
+using task14; 
+
+namespace task14tests
+{ 
+    public class IntegralTests 
+    { 
+        [Fact]
+        public void Test_ConstantFunction_SingleThread()
+        {
+            Assert.Equal(50, DefiniteIntegral.Solve(0, 10, x => 5, 1e-4, 1), 1e-3);
+        }
+
+        [Fact]
+        public void Test_QuadraticFunction_FourThreads()
+        {
+            Assert.Equal(8.0 / 3.0, DefiniteIntegral.Solve(0, 2, x => x * x, 1e-5, 4), 1e-4);
+        }
+
+        [Fact]
+        public void Test_LargeStep_ShouldStillWork()
+        {
+
+            double result = DefiniteIntegral.Solve(0, 1, x => x, 100.0, 2);
+            Assert.True(result > 0); 
+        }
+    } 
+}
